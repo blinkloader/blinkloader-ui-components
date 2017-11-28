65cd4e1019575fbbf1f1019df372565aa867330c
korshunad <korshunadd@gmail.com>
2017-11-28 18:44:39 +0700

    Update config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,5 @@
 import { configure } from '@storybook/react';
 
-/*function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
-}
-*/
 const req = require.context('../', true, /story\.jsx$/);
 
 function loadStories() {
